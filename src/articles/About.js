be99381785897ef6a6b608c398a2abd5aac09d72
astarryknight <whiteh4tter@gmail.com>
2024-07-22 20:25:44 -0400

    fix about me

--- src/articles/About.js
+++ src/articles/About.js
@@ -46,11 +46,11 @@ export default function About() {
         <Typography level='h1'>About</Typography>
       </Sheet>
       <Sheet id="content" sx={{width:'100%', height:'fit-content', my:'1em', mx:'1em', display:'flex', flexDirection:'row', justifyContent:'center'}}>
-        <Sheet id="leftContent" sx={{width:'60%', height:'fit-content', my:'1em', mx:'1em', display:'flex', flexDirection:'column', alignItems:'start'}}>
+        <Sheet id="leftContent" sx={{width:'40%', height:'fit-content', my:'1em', mx:'1em', display:'flex', flexDirection:'column', alignItems:'start'}}>
           <Typography level='h3'>Background</Typography>
           <Typography level='body-md'>Hi, I'm John Girgis. I have a passion for bringing ideas to life, whether through physical/mechanical and electrical design or through programming. Most of my physical projects have focused on being innovative solutions to modern problems (see self-tightening sneakers and ghost chessboard). For my programming projects, I also try to focus on solving problems in new ways, as the internet provides an amazing platform to share things through (see Coptic Wordle and Coptic Language/Learning Project).</Typography>
-          <Divider sx={{my:'2em'}}/>
-          <Typography level='h3'>Skills</Typography>
+          {/*<<Divider sx={{my:'2em'}}/>
+          Typography level='h3'>Skills</Typography>
           <div style={{width:"100%"}}>
             <table class="charts-css column hide-data show-4-secondary-axes show-primary-axis show-labels">
               <caption> Descriptive Line Chart Heading </caption>
@@ -85,10 +85,10 @@ export default function About() {
                 </tr>
               </tbody>
             </table>
-          </div>
+          </div>*/}
         </Sheet>
         <Divider orientation="vertical" />
-        <Sheet sx={{width:'30%', height:'fit-content', my:'1em', mx:'1em', display:'flex', flexDirection:'column', justifyContent:'center', alignItmes:'center' }}>
+        <Sheet sx={{width:'40%', height:'fit-content', my:'1em', mx:'1em', display:'flex', flexDirection:'column', justifyContent:'center', alignItmes:'center' }}>
           <AspectRatio ratio="5/6" variant="plain" sx={{ width:'100%', my:'1em', display:'flex', flexDirection:'column', justifyContent:'center', alignItmes:'center' }}>
             <Skeleton loading={true}>
 
